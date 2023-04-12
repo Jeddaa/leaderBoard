fc3ae9079b390bc781a8a13005c64aa380db8e01
jeddaa <fathiato@gmail.com>
2023-04-12 13:34:43 +0100

    making pr changes

--- src/index.js
+++ src/index.js
@@ -5,50 +5,6 @@ import addScore from './modules/addScore.js';
 import getScores from './modules/getScores.js';
 import refreshScore from './modules/refreshScore.js';
 
-// async function getGameId() {
-//   let myId;
-//   try {
-//     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         name: 'My new game',
-//       }),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//       },
-//     });
-//     const data = await response.json();
-//     myId = data;
-//   } catch (err) {
-//     return err;
-//   }
-//   return myId;
-// }
-
-// const id = getGameId().then((data) => {
-//   console.log(data);
-// });
-
-// async function getScores() {
-//   const data = await getGameId();
-//   try {
-//     const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${data}/scores/`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         user: 'Jane',
-//         score: 42,
-//       }),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//       },
-//     });
-//     const newData = await response.json();
-//     console.log(newData);
-//     return newData;
-//   } catch (e) {
-//     return e;
-//   }
-// }
 getGameId();
 getScores();
 addScore();
--- src/modules/getGameId.js
+++ src/modules/getGameId.js
@@ -1,5 +1,4 @@
-async function getGameId() {
+export default () => {
   const gameId = 'V0cpkV3hkx9r4XWa2c0x';
   localStorage.setItem('gameId', gameId);
-}
-export default getGameId;
+};
